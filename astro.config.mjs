a1407f84629d977e140f29d731eff3367fcf24a4
David Fricker <davidbfricker@gmail.com>
2023-01-01 22:15:47 -0500

    fix dns

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { fileURLToPath } from 'url';
 
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
-import { remarkReadingTime } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/config.mjs';
+import { remarkReadingTime } from './src/utils/frontmatter.mjs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -49,9 +49,15 @@ export default defineConfig({
 	},
 
 	vite: {
+		// define: {
+		// 	global: {}
+		// },
 		resolve: {
 			alias: {
 				'~': path.resolve(__dirname, './src'),
+				https: "https-browserify",
+				http: "http-browserify",
+				stream: "stream-browserify",
 			},
 		},
 	},
